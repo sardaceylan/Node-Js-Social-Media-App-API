10f74c900d3e7462beb6393de6988e0f31dfe33a
Arda <arda_ceylanrnwr@yahoo.com>
2022-11-28 17:03:19 +0300

    follower/following

--- routers/auth.js
+++ routers/auth.js
@@ -6,11 +6,12 @@ router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const user = new User({
+    const newUser = new User({
       username: req.body.username,
       email: req.body.email,
       password: hashedPassword,
     });
+    const user = await newUser.save();
     res.status(200).json(user);
   } catch (error) {
     console.log(error);
--- routers/users.js
+++ routers/users.js
@@ -38,15 +38,71 @@ router.delete("/:id", async (req, res) => {
       }
     }
     try {
-      const user = await User.findByIdAndDelete(req.params.id, {
-        $set: req.body,
-      });
+      await User.findByIdAndDelete({ _id: req.params.id });
       res.status(200).json("Hesap silindi.");
     } catch (error) {
       res.json(error);
     }
   } else {
     return res.status(403).json("Sadece kendi hesabını silebilirsin!");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const followingUser = await User.findById(req.params.id);
+      const followerUser = await User.findById(req.body.userId);
+      if (!followingUser.followers.includes(req.body.userId)) {
+        await followingUser.updateOne({
+          $push: { followers: req.body.userId },
+        });
+        await followerUser.updateOne({
+          $push: { followings: req.params.id },
+        });
+        res.status(200).json("kullanıcı takip edildi!");
+      } else {
+        res.status(403).json("bu kullanıcıyı zaten takip ediyorsun!");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("kendini takip edemezsin!");
+  }
+});
+
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const followingUser = await User.findById(req.params.id);
+      const followerUser = await User.findById(req.body.userId);
+      if (followingUser.followers.includes(req.body.userId)) {
+        await followingUser.updateOne({
+          $pull: { followers: req.body.userId },
+        });
+        await followerUser.updateOne({
+          $pull: { followings: req.params.id },
+        });
+        res.status(200).json("kullanıcı takipten çıkıldı!");
+      } else {
+        res.status(403).json("bu kullanıcıyı zaten takip etmiyorsun!");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("kendini takipten çıkamazsın!");
   }
 });
 
